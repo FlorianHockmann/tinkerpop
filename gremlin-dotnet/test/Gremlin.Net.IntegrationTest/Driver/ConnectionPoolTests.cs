69e41511f67697dda157ab99c638d4ba428a173e
Florian Hockmann <fh@florian-hockmann.de>
2018-12-20 16:44:57 +0100

    Add request pipelining TINKERPOP-1775/1774
    This allows connections to be reused for different requests in parallel
    which should lead to better utilization of connections / lower resource
    usage. The ConnectionPool now also has a fixed size whereas it could
    previously create an unlimited number of connections.
    Each Connection can handle up to MaxInProcessPerConnection requests in
    parallel.
    If this limit is reached for all connections, then a
    NoConnectionAvailableException is thrown which makes this a breaking
    change.

--- gremlin-dotnet/test/Gremlin.Net.IntegrationTest/Driver/ConnectionPoolTests.cs
+++ gremlin-dotnet/test/Gremlin.Net.IntegrationTest/Driver/ConnectionPoolTests.cs
@@ -25,6 +25,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Exceptions;
 using Gremlin.Net.IntegrationTest.Util;
 using Xunit;
 
@@ -39,52 +40,88 @@ namespace Gremlin.Net.IntegrationTest.Driver
         private async Task ExecuteMultipleLongRunningRequestsInParallel(IGremlinClient gremlinClient, int nrRequests,
             int requestRunningTimeInMs)
         {
-            var longRunningRequestMsg = _requestMessageProvider.GetSleepMessage(requestRunningTimeInMs);
             var tasks = new List<Task>(nrRequests);
             for (var i = 0; i < nrRequests; i++)
-                tasks.Add(gremlinClient.SubmitAsync(longRunningRequestMsg));
+            {
+                tasks.Add(gremlinClient.SubmitAsync(_requestMessageProvider.GetSleepMessage(requestRunningTimeInMs)));
+            }
+                
             await Task.WhenAll(tasks);
         }
 
-        [Fact]
-        public async Task ShouldReuseConnectionForSequentialRequests()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(8)]
+        public void ShouldCreateConfiguredNrConnections(int connectionPoolSize)
         {
-            var gremlinServer = new GremlinServer(TestHost, TestPort);
-            using (var gremlinClient = new GremlinClient(gremlinServer))
-            {
-                await gremlinClient.SubmitAsync("");
-                await gremlinClient.SubmitAsync("");
-
-                var nrConnections = gremlinClient.NrConnections;
-                Assert.Equal(1, nrConnections);
-            }
-        }
-
-        [Fact]
-        public void ShouldOnlyCreateConnectionWhenNecessary()
-        {
-            var gremlinServer = new GremlinServer(TestHost, TestPort);
-            using (var gremlinClient = new GremlinClient(gremlinServer))
+            using (var gremlinClient = CreateGremlinClient(connectionPoolSize))
             {
                 var nrConnections = gremlinClient.NrConnections;
-                Assert.Equal(0, nrConnections);
+                Assert.Equal(connectionPoolSize, nrConnections);
             }
         }
 
         [Fact]
         public async Task ShouldExecuteParallelRequestsOnDifferentConnections()
         {
-            var gremlinServer = new GremlinServer(TestHost, TestPort);
-            using (var gremlinClient = new GremlinClient(gremlinServer))
+            const int nrBlockingTasks = 1;
+            const int nrFastTasks = 1;
+            using (var gremlinClient = CreateGremlinClient(nrBlockingTasks + nrFastTasks, nrBlockingTasks + nrFastTasks))
             {
-                var sleepTime = 50;
-                var nrParallelRequests = 5;
+                const int sleepTime = 200;
 
-                await ExecuteMultipleLongRunningRequestsInParallel(gremlinClient, nrParallelRequests, sleepTime);
+                var blockingConnectionTask =
+                    ExecuteMultipleLongRunningRequestsInParallel(gremlinClient, nrBlockingTasks, sleepTime);
+                await Task.Delay(50);
+                var fastTask = ExecuteMultipleFastRequestsInParallel(gremlinClient, nrFastTasks);
 
-                var nrConnections = gremlinClient.NrConnections;
-                Assert.Equal(nrParallelRequests, nrConnections);
+                var completedTask =
+                    await WaitForTaskOrTimeoutAsync(fastTask, TimeSpan.FromMilliseconds(sleepTime - 10));
+                await fastTask;
+                await blockingConnectionTask;
+                Assert.Equal(fastTask, completedTask);
             }
+        }
+
+        private async Task ExecuteMultipleFastRequestsInParallel(IGremlinClient gremlinClient, int nrRequests)
+        {
+            var tasks = new List<Task>(nrRequests);
+            for (var i = 0; i < nrRequests; i++)
+            {
+                tasks.Add(gremlinClient.SubmitAsync(_requestMessageProvider.GetDummyMessage()));
+            }
+                
+            await Task.WhenAll(tasks);
+        }
+
+        [Fact]
+        public async Task ShouldThrowNoConnectionAvailableExceptionWhenNoConnectionIsAvailable()
+        {
+            const int nrParallelRequests = 2;
+            using (var gremlinClient = CreateGremlinClient(connectionPoolSize: 1, maxInProcessPerConnection: 1))
+            {
+                const int sleepTime = 100;
+
+                await Assert.ThrowsAsync<NoConnectionAvailableException>(() =>
+                    ExecuteMultipleLongRunningRequestsInParallel(gremlinClient, nrParallelRequests, sleepTime));
+            }
+        }
+
+        private static GremlinClient CreateGremlinClient(int connectionPoolSize = 2, int maxInProcessPerConnection = 4)
+        {
+            var gremlinServer = new GremlinServer(TestHost, TestPort);
+            return new GremlinClient(gremlinServer,
+                connectionPoolSettings: new ConnectionPoolSettings
+                {
+                    PoolSize = connectionPoolSize,
+                    MaxInProcessPerConnection = maxInProcessPerConnection
+                });
+        }
+
+        private static Task<Task> WaitForTaskOrTimeoutAsync(Task task, TimeSpan timeout)
+        {
+            return Task.WhenAny(task, Task.Delay(timeout));
         }
     }
 }
